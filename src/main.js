090f64965c690e0d8aa1357fa0df7c334be9f91d
Micael Santiago <83787168+micaelsantiago@users.noreply.github.com>
2024-02-02 20:03:37 -0300

    raw: integrating socket.io

--- src/main.js
+++ src/main.js
@@ -3,14 +3,22 @@ require('dotenv').config();
 const express = require('express');
 const { createServer } = require('node:http');
 const { join } = require('node:path');
+const { Server } = require('socket.io');
 
+const PORT = process.env.PORT || 3000;
 const app = express();
 const server = createServer(app);
-const PORT = process.env.PORT || 3000;
+const io = new Server(server);
 
-app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, '../public/index.html'));
-});
+app.use(express.static(join(__dirname, '..', 'public')));
+
+io.on('connection', socket => {
+  console.log('User: ', socket.id);
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected: ', socket.id);
+  });
+})
 
 server.listen(PORT, () => {
   console.log('server running at http://localhost:3000');
